041946771d3f83f366f21d7baf4e9b9cc02ad273
coolkidneversleep <iiippbhy@gmail.com>
2021-07-21 21:30:49 +0700

    add works part

--- gatsbynetlify/src/pages/index.js
+++ gatsbynetlify/src/pages/index.js
@@ -4,6 +4,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Header from "../components/header"
+import WorksPage from "./works"
 import '../images/profile.jpg'
 import '../styles/index.css'
 import { useRef, useEffect } from "react"
@@ -11,10 +12,10 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const IndexPage = () => {
-  gsap.set(".circle", { yPercent: 5 });
-  gsap.set(".dotsBlue", { yPercent: 10 });
+  gsap.set(".circle", { yPercent: -15 });
+  gsap.set(".dotsBlue", { yPercent: 20 });
   gsap.to(".circle", {
-    yPercent: -5,
+    yPercent: 15,
     ease: "none",
     scrollTrigger: {
       trigger: ".clusterGreat",
@@ -23,7 +24,7 @@ const IndexPage = () => {
   });
 
   gsap.to(".dotsBlue", {
-    yPercent: -10,
+    yPercent: -20,
     ease: "none",
     scrollTrigger: {
       trigger: ".clusterGreat",
@@ -101,11 +102,12 @@ const IndexPage = () => {
     <div className="App" ref={ref}>
       <Header />
       <div style={{ display: "grid", gridTemplateRows: "auto" }}>
-        <div style={{ display: "grid", gridTemplateColumns: "50% 50%" }}>
+        <div style={{ display: "grid", gridTemplateColumns: "60% 40%" }}>
           <div className="txt_block">
-            <h1 className="great_txt">hello there,</h1><br></br>
+            <h1 className="great_txt">hi there,</h1><br></br>
             <h1 className="name_txt">I'm kaewket </h1>
             <h2 className="role_txt">Computer Science Student, KMUTT</h2>
+            <h3 className="txt"> I am studying as	an undergraduate in SIT at KMUTT. I like to create web applications that has good user-interface and user-experience. I always open my mind and willing to learn new things to achieve the goals.</h3>
           </div>
           <section class="cluster clusterGreat">
             <div class="circle clusterPieces"></div>
@@ -114,7 +116,8 @@ const IndexPage = () => {
         </div>
 
       </div>
-      <img src="https://scontent.fbkk2-8.fna.fbcdn.net/v/t1.6435-9/101483980_3058963557524210_3918084869349652298_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=iRyX4NX5jCoAX9Gu2ze&_nc_ht=scontent.fbkk2-8.fna&oh=400adc2e0306aac2eb7eca8846d9ea0d&oe=60FD47D5"></img>
+      <WorksPage />
+      {/* <img src="https://scontent.fbkk2-8.fna.fbcdn.net/v/t1.6435-9/101483980_3058963557524210_3918084869349652298_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=iRyX4NX5jCoAX9Gu2ze&_nc_ht=scontent.fbkk2-8.fna&oh=400adc2e0306aac2eb7eca8846d9ea0d&oe=60FD47D5"></img> */}
     </div>
 
   )
--- gatsbynetlify/src/pages/works.js
+++ gatsbynetlify/src/pages/works.js
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { Typography } from '@material-ui/core';
+import { Container, Typography } from '@material-ui/core';
 import Header from '../components/header';
+import '../styles/works.css'
 
 const WorksPage = () => {
     return (
-      <Header />
+      <Container maxWidth="lg" style={{backgroundColor:"#F7F3E9"}}>
+        <h2 className="heading">All of <br></br>My works</h2>
+      </Container>
+
     )
   }
 export default WorksPage
