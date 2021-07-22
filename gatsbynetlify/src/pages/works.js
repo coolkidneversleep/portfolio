669490cdd2eea2a1ac383daec0d96d2326609d94
coolkidneversleep <iiippbhy@gmail.com>
2021-07-22 15:38:33 +0700

    add: animation scroll zoom to works

--- gatsbynetlify/src/pages/works.js
+++ gatsbynetlify/src/pages/works.js
@@ -1,37 +1,90 @@
 import * as React from 'react';
 import { Container } from '@material-ui/core';
 import eduroom from '../images/eduroom.png';
-import { gsap } from "gsap";
-import { ScrollMagic } from "gsap/ScrollTrigger";
+import vcare from '../images/vcare.png';
 import '../styles/works.css'
-import { TimelineMax } from 'gsap/gsap-core';
+import { gsap } from "gsap";
 
 const WorksPage = () => {
-  var controller = new ScrollMagic.Controller();
+  let scaleAmount = 0.25;
 
-  $(".wrapper").each(function() {
-    var tl = new TimelineMax();
-    var cov = $(this).find(".cover");
-    var img = $(this).find("img");
+function scrollZoom() {
+  const images = document.querySelectorAll("[data-scroll-zoom]");
+  let scrollPosY = 0;
+  scaleAmount = scaleAmount / 100;
 
-    tl.from(cov, 1, { scaleX: 0, transformOrigin: "left" });
-    tl.to(cov, 1, { scaleX: 0, transformOrigin: "right" }, "reveal");
-    tl.from(img, 1, { opacity: 0 }, "reveal");
+  const observerConfig = {
+    rootMargin: "0% 0% 0% 0%",
+    threshold: 0
+  };
 
-    var scene = new ScrollMagic.Scene({
-      triggerElement: this,
-      triggerHook: 0.7
-    })
-      .setTween(tl)
-      .addIndicators()
-      .addTo(controller);
+  // Create separate IntersectionObservers and scroll event listeners for each image so that we can individually apply the scale only if the image is visible
+  images.forEach(image => {
+    let isVisible = false;
+    const observer = new IntersectionObserver((elements, self) => {
+      elements.forEach(element => {
+        isVisible = element.isIntersecting;
+      });
+    }, observerConfig);
+
+    observer.observe(image);
+
+    // Set initial image scale on page load
+    image.style.transform = `scale(${1 + scaleAmount * percentageSeen(image)})`;
+
+    // Only fires if IntersectionObserver is intersecting
+    window.addEventListener("scroll", () => {
+      if (isVisible) {
+        scrollPosY = window.pageYOffset;
+        image.style.transform = `scale(${1 +
+          scaleAmount * percentageSeen(image)})`;
+      }
+    });
   });
+
+  // Calculates the "percentage seen" based on when the image first enters the screen until the moment it leaves
+  // Here, we get the parent node position/height instead of the image since it's in a container that has a border, but
+  // if your container has no extra height, you can simply get the image position/height
+  function percentageSeen(element) {
+    const parent = element.parentNode;
+    const viewportHeight = window.innerHeight;
+    const scrollY = window.scrollY;
+    const elPosY = parent.getBoundingClientRect().top + scrollY;
+    const borderHeight = parseFloat(getComputedStyle(parent).getPropertyValue('border-bottom-width')) + parseFloat(getComputedStyle(element).getPropertyValue('border-top-width'));
+    const elHeight = parent.offsetHeight + borderHeight;
+
+    if (elPosY > scrollY + viewportHeight) {
+      // If we haven't reached the image yet
+      return 0;
+    } else if (elPosY + elHeight < scrollY) {
+      // If we've completely scrolled past the image
+      return 100;
+    } else {
+      // When the image is in the viewport
+      const distance = scrollY + viewportHeight - elPosY;
+      let percentage = distance / ((viewportHeight + elHeight) / 100);
+      percentage = Math.round(percentage);
+
+      return percentage;
+    }
+  }
+}
+
+scrollZoom();
+
+
     return (
       <Container maxWidth="lg" style={{backgroundColor:"#f8f8ff"}}>
-        <h2 className="heading">All of <br></br>My works</h2>
-        <div className="wrapper">
-          <img src={eduroom} width="600" height="450"></img>
-          <div className="cover"></div>
+        <div style={{display:"grid", gridTemplateColumns:"45% 55%"}}>
+            <div>
+              <h2 className="heading">All of <br></br>My works</h2>
+              <div className="image">
+                <img src={eduroom} data-scroll-zoom width="550" height="400" style={{ borderRadius:"20px",objectFit:"cover"}}></img>
+              </div>
+            </div>
+            <div className="image" style={{paddingLeft:"120px", paddingTop:"50px"}}>
+              <img src={vcare} data-scroll-zoom width="550" height="400" style={{borderRadius:"20px",objectFit:"cover"}}></img>
+            </div>
         </div>
       </Container>
 
