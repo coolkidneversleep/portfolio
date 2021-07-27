7a4da64c4846f6903a7a79b689acb17387e43f41
coolkidneversleep <iiippbhy@gmail.com>
2021-07-27 17:15:16 +0700

    add bg image on landing page

--- gatsbynetlify/src/pages/index.js
+++ gatsbynetlify/src/pages/index.js
@@ -5,121 +5,33 @@ import AboutIntro from "./about_int"
 import Footer from '../components/footer'
 import '../images/profile.jpg'
 import '../styles/index.css'
-import opal from '../images/opal2.png';
-import { useRef, useEffect } from "react"
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 
 
 
 const IndexPage = () => {
-  gsap.set(".circle", { yPercent: -15 });
-  gsap.set(".dotsBlue", { yPercent: 20 });
-  gsap.to(".circle", {
-    yPercent: 25,
-    ease: "none",
-    scrollTrigger: {
-      trigger: ".clusterGreat",
-      scrub: 1
-    },
-  });
 
-  gsap.to(".dotsBlue", {
-    yPercent: -10,
-    ease: "none",
-    scrollTrigger: {
-      trigger: ".clusterGreat",
-      scrub: 1
-    },
-  });
-
-  gsap.to(".clusterGreat", {
-    yPercent: -5,
-    ease: "none",
-    scrollTrigger: {
-      trigger: ".clusterGreat",
-      end: "bottom center",
-      scrub: 1
-    },
-  });
-  gsap.registerPlugin(ScrollTrigger);
-  const ref = useRef(null);
-  useEffect(() => {
-    const element = ref.current;
-    gsap.fromTo(
-      element.querySelector(".great_txt"),
-      {
-        opacity: 1,
-        y: -20
-      },
-      {
-        opacity: 0,
-        y: 0,
-        scrollTrigger: {
-          trigger: element.querySelector(".txt_block"),
-          start: "top top",
-          end: "bottom center",
-          scrub: true
-        }
-      }
-    );
-    gsap.fromTo(
-      element.querySelector(".name_txt"),
-      {
-        opacity: 1,
-        y: -20
-      },
-      {
-        opacity: 0,
-        y: 0,
-        scrollTrigger: {
-          trigger: element.querySelector(".txt_block"),
-          start: "top top",
-          end: "bottom center",
-          scrub: true
-        }
-      }
-    );
-    gsap.fromTo(
-      element.querySelector(".role_txt"),
-      {
-        opacity: 1,
-        y: -20
-      },
-      {
-        opacity: 0,
-        y: 0,
-        scrollTrigger: {
-          trigger: element.querySelector(".txt_block"),
-          start: "top top",
-          end: "bottom center",
-          scrub: true
-        }
-      }
-    );
-  }, []);
   return (
-    <div className="App" ref={ref}>
-      <Header />
-      <div style={{ display: "grid", gridTemplateRows: "auto" }}>
+    <div className="App">
+      <Header/>
+      <div style={{ display: "grid", gridTemplateRows: "auto" }} className="landing">
         <div style={{ display: "grid", gridTemplateColumns: "60% 40%"}}>
           <div className="txt_block">
-            <h1 className="great_txt">Hi there,</h1><br></br>
+            <h1 className="great_txt">hi there,</h1><br></br>
             <h1 className="name_txt">I'm kaewket </h1>
             <h2 className="role_txt">Computer Science Student, KMUTT</h2>
-            <img src={opal} width="423px" height="423px" style={{marginTop:"120px", marginLeft:"400px"}}></img>
           </div>
-          <section  class="cluster clusterGreat">
+          {/* <section  class="cluster clusterGreat">
             <div class="circle clusterPieces"></div>
-            <div style={{paddingLeft:"40px"}}><img  class="dotsBlue clusterPieces" src="https://www.micelistudios.com/sandbox/scrolltrigger/imgs/dots_blue_494x434.svg" alt="blue dot"/></div>
+            <div><img  class="dotsBlue clusterPieces" src="https://www.micelistudios.com/sandbox/scrolltrigger/imgs/dots_blue_494x434.svg" alt="blue dot"/></div>
             
             
-          </section>
+          </section> */}
         </div>
 
       </div>
       <WorksPage/>
-      <div data-sal="slide-up" data-sal-duration="2000" data-sal-delay="5" data-sal-easing="easeOutBack">
+      <div data-sal="slide-up" data-sal-duration="2000" data-sal-delay="2" data-sal-easing="easeOutBack">
         <AboutIntro/> 
       </div>
       <Footer />
